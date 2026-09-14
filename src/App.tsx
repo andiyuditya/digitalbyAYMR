9cc05265ae169ae997e655fd5ed0b17bb0cf6283
andiyuditya <160710505+andiyuditya@users.noreply.github.com>
2026-09-14 16:25:45 +0700

    Fix router base path for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { LoadingProvider } from "./context/LoadingProvider";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/digitalbyAYMR">
       <Routes>
         <Route
           path="/"
